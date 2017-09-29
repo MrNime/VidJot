cd514901037d6638cee8154f9312a794d3e06038
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-09-29 23:21:23 +0200

    Check duplicate username or email

--- routes/users.js
+++ routes/users.js
@@ -25,48 +25,60 @@ router.post('/register', (req, res) => {
     if (req.body.password !== req.body.password2) {
         errors.push({ text: 'Passwords do not match' });
     }
+
     if (req.body.password.length < 4) {
         errors.push({ text: 'Password must be at least 4 characters' });
     }
 
-    if (errors.length > 0) {
-        res.render('users/register', {
-            errors: errors,
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            password2: req.body.password2
-        });
-    } else {
-        let newUser = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        };
-
-        bcrypt.genSalt(10, (err, salt) => {
-            // linter expects error to be handled
-            if (err) {
-                console.log(err);
+    let dupeMail = User.findOne({ email: req.body.email });
+    let dupeName = User.findOne({ name: req.body.name });
+    Promise.all([dupeMail, dupeName])
+        .then((array) => {
+            if (array[0]) {
+                errors.push({ text: 'Email already registered' });
             }
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) {
-                    throw err;
-                }
-                newUser.password = hash;
-                console.log(newUser);
-                new User(newUser)
-                    .save()
-                    .then(idea => {
-                        req.flash('success_msg', 'You are now registered and can log in');
-                        res.redirect('/users/login');
-                    })
-                    .catch(err => {
+            if (array[1]) {
+                errors.push({ text: 'Name already taken' });
+            }
+            if (errors.length > 0) {
+                res.render('users/register', {
+                    errors: errors,
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    password2: req.body.password2
+                });
+            } else {
+                let newUser = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                };
+                bcrypt.genSalt(10, (err, salt) => {
+                    // linter expects error to be handled
+                    if (err) {
                         console.log(err);
+                    }
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log('pre-encrypt' + JSON.stringify(newUser));
+                        newUser.password = hash;
+                        console.log('post-encrypt' + JSON.stringify(newUser));
+                        new User(newUser)
+                            .save()
+                            .then(idea => {
+                                req.flash('success_msg', 'You are now registered and can log in');
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            });
                     });
-            });
+                });
+            };
         });
-    }
 });
 
 module.exports = router;

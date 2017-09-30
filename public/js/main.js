22353a949f9cc5504dc8d0040dbf4a9e4cbb83a5
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-10-01 01:12:01 +0200

    rebrand

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
     $('.alert.alert-success').fadeOut(5000);
 });
+
+// snippet from https://codepen.io/vsync/pen/frudD
+// Applied globally on all textareas with the "autoExpand" class
+$(document)
+.one('focus.autoExpand', 'textarea.autoExpand', function(){
+    var savedValue = this.value;
+    this.value = '';
+    this.baseScrollHeight = this.scrollHeight;
+    this.value = savedValue;
+})
+.on('input.autoExpand', 'textarea.autoExpand', function(){
+    var minRows = this.getAttribute('data-min-rows')|0, rows;
+    this.rows = minRows;
+    rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+    this.rows = minRows + rows;
+});
